8cf13037d6b6789885bbae9cc29b7804468d0b18
David <davreixach@gmail.com>
2024-08-17 13:09:51 +0200

    minor

--- make_figures.py
+++ make_figures.py
@@ -35,10 +35,6 @@ def extract_images_from_summary(events_path, tag_names_to_look_for, suffix='', i
         tag_name = tag.replace('/', '_')
         # ... that have the tag name: "tag_name_to_look_for"
 
-        print(event_acc.Tags()['images'])
-        print(tag_names_to_look_for)
-        quit()
-
         if tag_name in tag_names_to_look_for:
             tag_name = tag_name + strsuffix
 
--- make_figures_inpainting.py
+++ make_figures_inpainting.py
@@ -2,4 +2,4 @@ import make_figures
 
 make_figures.extract_image_psnrs(dict({'base': '/home/david/Research/Tensor/siren/logs/experiment_7/summaries/'}))
 
-make_figures.extract_images_from_summary('/home/david/Research/Tensor/siren/logs/experiment_7/summaries/', 'train_pred_image', suffix='', img_outdir='out/', colormap=None)
+make_figures.extract_images_from_summary('/home/david/Research/Tensor/siren/logs/experiment_7/summaries/', 'train_pred_img', suffix='', img_outdir='out/', colormap=None)
